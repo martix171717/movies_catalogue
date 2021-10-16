623c5b17c6d3ae1c2390be95e5898c70193bcd70
Martyna Szymańska <martix171717@gmail.com>
2021-10-16 07:22:15 +0200

    Update tmdb_client.py

--- tmdb_client.py
+++ tmdb_client.py
@@ -1,6 +1,7 @@
 import requests
 import random
 
+
 API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MjJjNzc0NDZmZDA0NDI3NmI1YjNkMTgzN2FiNzc0ZSIsInN1YiI6IjYxNjE2YWU2ODlkOTdmMDAyOTExMGIyOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QysD4IfN6R-qX8HsR1ejvMwSfPWkmfymAb4mRDJvha8"
 
 def get_popular_movies():
@@ -18,6 +19,7 @@ def get_poster_url(poster_api_path, size="w342"):
 def get_movies(how_many,list_type):
     data = get_movies_list(list_type)
     return data["results"][:how_many]
+
 
 def get_single_movie(movie_id):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
