bb0d65ecf6b3417ce492d11040da5f00b108affc
Martyna Szymańska <martix171717@gmail.com>
2021-10-16 07:21:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import tmdb_client
 import random
 
@@ -6,9 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
+    types=['popular', 'now_playing', 'top_rated', 'upcoming']
     selected_list = request.args.get('list_type', "popular")
     movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
-    return render_template("homepage.html", movies=movies, current_list=selected_list)
+    return render_template("homepage.html", movies=movies, selected_list=selected_list, types=types)
 
 @app.context_processor
 def utility_processor():
@@ -24,7 +25,6 @@ def movie_details(movie_id):
    movie_images = tmdb_client.get_movie_images(movie_id)
    selected_backdrop = random.choice(movie_images['backdrops'])
    return render_template("movie_details.html", movie=details, cast=cast, selected_backdrop=selected_backdrop)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
