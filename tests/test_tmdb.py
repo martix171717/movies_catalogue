e8d37fa96d6858aa2a92cccabf2c8a5d910e8c7d
Martyna Szymańska <martix171717@gmail.com>
2021-11-08 20:13:04 +0100

    Added test_homepage and pytest.mark.parametrize

--- tests/test_tmdb.py
+++ tests/test_tmdb.py
@@ -54,3 +54,21 @@ def test_get_movie_images(monkeypatch):
     monkeypatch.setattr("tmdb_client.call_tmdb_api", call_tmdb_api_mock)
     movie_images = tmdb_client.get_movie_images(movie_id=1)
     assert movie_images == mock_movie_images
+
+@pytest.mark.parametrize('list_type', (
+    ('movie/now_playing'),
+    ('movie/top_rated'),
+    ('movie/upcoming'),
+    ('movie/popular')
+))
+def test_homepage(monkeypatch, list_type):
+    api_mock = Mock(return_value={'results': []})
+    monkeypatch.setattr("tmdb_client.call_tmdb_api", api_mock)
+
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
+        api_mock.assert_called_once_with('movie/popular')
+
+
+
