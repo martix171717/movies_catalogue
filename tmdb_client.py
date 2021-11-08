f5b62d041a8051a6251e8fce987c3c9044edfdd3
Martyna Szymańska <martix171717@gmail.com>
2021-11-08 12:43:39 +0100

    Add call_tmdb_api function

--- tmdb_client.py
+++ tmdb_client.py
@@ -2,6 +2,15 @@ import requests
 
 API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MjJjNzc0NDZmZDA0NDI3NmI1YjNkMTgzN2FiNzc0ZSIsInN1YiI6IjYxNjE2YWU2ODlkOTdmMDAyOTExMGIyOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QysD4IfN6R-qX8HsR1ejvMwSfPWkmfymAb4mRDJvha8"
 
+def call_tmdb_api(endpoint):
+   full_url = f"https://api.themoviedb.org/3/{endpoint}"
+   headers = {
+       "Authorization": f"Bearer {API_TOKEN}"
+   }
+   response = requests.get(full_url, headers=headers)
+   response.raise_for_status()
+   return response.json()
+
 def get_popular_movies():
     endpoint = "https://api.themoviedb.org/3/movie/popular"
     headers = {
@@ -19,28 +28,13 @@ def get_movies(how_many,list_type):
     return data["results"][:how_many]
 
 def get_single_movie(movie_id):
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
-    response = requests.get(endpoint, headers=headers)
-    return response.json()
+    return call_tmdb_api(f"movie/{movie_id}")
 
 def get_single_movie_cast(movie_id):
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
-    response = requests.get(endpoint, headers=headers)
-    return response.json()["cast"]
+    return call_tmdb_api(f"movie/{movie_id}/credits")["cast"]
 
 def get_movie_images(movie_id):
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
-    response = requests.get(endpoint, headers=headers)
-    return response.json()
+    return call_tmdb_api(f"movie/{movie_id}/images")
 
 def get_movies_list(list_type):
     endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
